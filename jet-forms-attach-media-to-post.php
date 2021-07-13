e4ad4a6c3ac52e24cdd5a727198036c1820565f2
girafffee <46720998+girafffee@users.noreply.github.com>
2021-07-13 18:02:55 +0300

    ADD: jfb compatibility

--- jet-forms-attach-media-to-post.php
+++ jet-forms-attach-media-to-post.php
@@ -13,11 +13,150 @@
  */
 
 // If this file is called directly, abort.
+use Jet_Form_Builder\Actions\Action_Handler;
+
 if ( ! defined( 'WPINC' ) ) {
 	die();
 }
 
 add_action( 'jet-engine/forms/booking/notification/insert_post', 'jet_forms_attach_media_to_post', 20, 2 );
+
+add_action( 'jet-form-builder/action/after-post-insert', 'jet_forms_attach_media_to_post_jfb', 20, 2 );
+
+function _jet_forms_attach_images( $request_source, $media_keys, $inserted_post_id ) {
+	$get_attachment_image_data_array = function( $img_data = null, $include = 'all' ) {
+		$result = false;
+
+		if ( empty( $img_data ) ) {
+			return $result;
+		}
+
+		if ( is_numeric( $img_data ) ) {
+
+			switch ( $include ) {
+				case 'id':
+					$result = array(
+						'id' => $img_data,
+					);
+					break;
+
+				case 'url':
+					$result = array(
+						'url' => wp_get_attachment_url( $img_data ),
+					);
+					break;
+
+				default:
+					$result = array(
+						'id'  => $img_data,
+						'url' => wp_get_attachment_url( $img_data ),
+					);
+			}
+
+		} elseif ( filter_var( $img_data, FILTER_VALIDATE_URL ) ) {
+
+			switch ( $include ) {
+				case 'id':
+					$result = array(
+						'id' => attachment_url_to_postid( $img_data ),
+					);
+					break;
+
+				case 'url':
+					$result = array(
+						'url' => $img_data,
+					);
+					break;
+
+				default:
+					$result = array(
+						'id'  => attachment_url_to_postid( $img_data ),
+						'url' => $img_data,
+					);
+			}
+
+		} elseif ( is_array( $img_data ) && isset( $img_data['id'] ) && isset( $img_data['url'] ) ) {
+
+			switch ( $include ) {
+				case 'id':
+					$result = array(
+						'id' => $img_data['id'],
+					);
+					break;
+
+				case 'url':
+					$result = array(
+						'url' => $img_data['url'],
+					);
+					break;
+
+				default:
+					$result = $img_data;
+			}
+
+		}
+
+		return $result;
+	};
+
+	foreach ( $media_keys as $media_key ) {
+
+		if ( empty( $request_source[ $media_key ] ) ) {
+			continue;
+		}
+
+		$attachment_data = $request_source[ $media_key ];
+
+		if ( ! is_array( $attachment_data ) ) {
+			$attachment_data = explode( ',', $request_source[ $media_key ] );
+		}
+
+		$attachment_data = isset( $attachment_data['id'] ) ? array( $attachment_data ) : $attachment_data;
+
+		$attachment_ids = array_map( function ( $item ) use ( $get_attachment_image_data_array ) {
+			return call_user_func( $get_attachment_image_data_array, $item, 'id' );
+		}, $attachment_data );
+		
+		$attachment_ids = wp_list_pluck( $attachment_ids, 'id' );
+
+		foreach ( $attachment_ids as $attachment_id ) {
+			wp_update_post( array(
+				'ID'          => $attachment_id,
+				'post_parent' => $inserted_post_id,
+			) );
+		}
+	}
+};
+
+/**
+ * @param $action
+ * @param Action_Handler $handler
+ */
+function jet_forms_attach_media_to_post_jfb( $action, $handler ) {
+
+	$inserted_id = $handler->get_inserted_post_id();
+
+	if ( empty( $inserted_id ) ) {
+		return;
+	}
+	$field_settings = wp_list_pluck( jet_form_builder()->form_handler->request_handler->_fields, 'attrs' );
+	$media_keys = array();
+
+	foreach ( $field_settings as $field_setting ) {
+		if ( isset( $field_setting['insert_attachment'] )
+		     && isset( $field_setting['name'] )
+		     && filter_var( $field_setting['insert_attachment'], FILTER_VALIDATE_BOOLEAN )
+		) {
+			$media_keys[] = $field_setting['name'];
+		}
+	}
+
+	if ( empty( $media_keys ) ) {
+		return;
+	}
+
+	call_user_func( '_jet_forms_attach_images', $handler->request_data, $media_keys, $inserted_id );
+}
 
 function jet_forms_attach_media_to_post( $notification, $manager ) {
 
@@ -51,31 +190,5 @@ function jet_forms_attach_media_to_post( $notification, $manager ) {
 		return;
 	}
 
-	foreach ( $media_keys as $media_key ) {
-
-		if ( empty( $manager->data[ $media_key ] ) ) {
-			continue;
-		}
-
-		$attachment_data = $manager->data[ $media_key ];
-
-		if ( ! is_array( $attachment_data ) ) {
-			$attachment_data = explode( ',', $manager->data[ $media_key ] );
-		}
-
-		$attachment_data = isset( $attachment_data['id'] ) ? array( $attachment_data ) : $attachment_data;
-
-		$attachment_ids = array_map( function ( $item ) {
-			return Jet_Engine_Tools::get_attachment_image_data_array( $item, 'id' );
-		}, $attachment_data );
-
-		$attachment_ids = wp_list_pluck( $attachment_ids, 'id' );
-
-		foreach ( $attachment_ids as $attachment_id ) {
-			wp_update_post( array(
-				'ID'          => $attachment_id,
-				'post_parent' => $manager->data['inserted_post_id'],
-			) );
-		}
-	}
+	call_user_func( '_jet_forms_attach_images', $manager->data, $media_keys, $manager->data['inserted_post_id'] );
 }
